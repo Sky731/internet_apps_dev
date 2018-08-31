cd0973fb4f57517d87b29fa63a3646fc3361b879
Sky731 <Ivan4784@mail.ru>
2018-09-01 02:11:01 +0300

    php script and js works fine

--- lab1/validator.php
+++ lab1/validator.php
@@ -1,11 +1,13 @@
 <?php
 
+$time_start = microtime(true);
+
 $y = $_POST["y"];
 $x = $_POST["x"];
 $r = $_POST["r"];
 
 if (!is_numeric($y) || !is_numeric($x) || !is_numeric($r)) {
-    echo "{result: 'ZHOPA'}";
+    echo "{result: 'ZHOPA'}"; // FIXME which fields is invalid ?
     exit;
 }
 
@@ -23,13 +25,43 @@ if ($r <= 2 || $r >= 5) {
     array_push($invalid, "r");
 }
 
-if ($y % 1 != 0 || $y <= -3 || $y >= 5) {
+if ($y % 1 != 0 || $y < -3 || $y > 5) {
     array_push($invalid, "y");
 }
-
 
 if (count($invalid) != 0) {
     $response = array("result" => "invalid", "values" => $invalid);
     echo json_encode($response);
     exit;
 }
+
+function checkHit($x, $y, $r) {
+    if ($x >= 0 && $y >= 0) {
+        if ($x <= $r && $y <= $r) {
+            return true;
+        } else return false;
+    } elseif ($x >= 0 && $y < 0) {
+        if (abs($x) + abs($y) <= $r/2) {
+            return true;
+        } else return false;
+
+    } elseif ($x < 0 && $y <= 0) {
+        if (sqrt($x*$x + $y*$y) <= $r/2) {
+            return true;
+        } else return false;
+    } else return false;
+}
+
+date_default_timezone_set('Europe/Moscow');
+$time_end = microtime(true);
+$exec_time = $time_end - $time_start;
+
+$response = array("result" => "valid",
+                  "x" => $x,
+                  "y" => $y,
+                  "r" => $r,
+                  "is_in" => checkHit($x, $y, $r),
+                  "cur_time" => date('H:i:s', time()),
+                  "exec_time" => $exec_time);
+
+echo json_encode($response);
